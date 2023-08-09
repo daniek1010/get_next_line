cf5a07b1c6c83746b59fba8135c45cb49e25f671
danevans <danevans@student.42wolfsburg.de>
2023-08-09 12:13:11 +0200

    first time

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: danevans <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/18 08:27:38 by danevans          #+#    #+#             */
+/*   Updated: 2023/07/22 12:40:41 by danevans         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+char	*ft_strdup(const char *str);
+char	*ft_strjoin(const char *dest, const char *src);
+char	*ft_strchr(const char *str, int c);
+#endif
